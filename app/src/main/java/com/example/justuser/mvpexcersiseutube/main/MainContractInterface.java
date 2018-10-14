bca2dbb6c6416c7cf70c2671cf8b2d1838216617
bisratf <habesha45@gmail.com>
2018-10-14 16:16:38 +0100

    Unit Test was added

--- app/src/main/java/com/example/justuser/mvpexcersiseutube/main/MainContractInterface.java
+++ app/src/main/java/com/example/justuser/mvpexcersiseutube/main/MainContractInterface.java
@@ -1,9 +1,16 @@
-package com.example.justuser.mvpexcersiseutube;
+package com.example.justuser.mvpexcersiseutube.main;
+
+
+/**
+ * Contract between the View {@link MainActivity } and presenter {@link MainPresenter}
+ */
+
 
 public interface MainContractInterface {
 
      interface  MVPView {
          void  showSignInScreen();
+
          void showSignUpScreen();
      }
      interface  Presenter {
